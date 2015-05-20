1789250f7ea2c858ec1d82fbced3435b3e4061e6
Riobans <riobans.urra@inacapmail.cl>
2015-05-20 16:09:42 -0300

    Tercer envío
    correcciones de ortografía y asignaturas

--- MenuConsultas/src/menuconsultas/MenuConsultas.java
+++ MenuConsultas/src/menuconsultas/MenuConsultas.java
@@ -20,18 +20,20 @@ public class MenuConsultas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner captura =new Scanner(System.in);  
+// TODO code application logic here
+     
+        
+        
+     Scanner captura =new Scanner(System.in);  
 
-     //Variables de ingreso al sistema
+     
      String Nombre; 
      int opcion ;
      int Alumno= 1;
      int semestre;
      int cantidad;
      
-     //Variables usadas para calcular promedios
-     //Variables usadas para calcular promedios
+     
      double promedio1 = 0;
      double promedio2 = 0;
      double promedio3 = 0;
@@ -47,20 +49,20 @@ public class MenuConsultas {
      double promediosemestre1;
      double promediosemestre2;
      
-    //Inicio del programa
-     System.out.println("----Bienvenido a Intranet-------");
-     System.out.print("Ingrese Nombre : ");
+//Inicio del programa
+     System.out.println("----Bienvenido a Intranet Alumno-------");
+     System.out.print("Ingrese su nombre : ");
      Nombre=captura.nextLine();
     
-    //se valida si es alumno o profesor
-     System.out.println("Ingrese si es alumno o profesor : ");
-     System.out.println("1._Alumno/a");
-     System.out.println("2._Profesor/a");
+//se valida si es o no alumno
+     System.out.println("¿Es usted alumno/a? : ");
+     System.out.println("1._Sí");
+     System.out.println("2._No");
      System.out.print  ("Digite la opción: ");
      opcion=captura.nextInt();
      
      
-    //si es alumno     
+//si es alumno (Estructura de decición doble)     
       if(opcion==Alumno){
          System.out.println();
          System.out.println("---Bienvenido Alumno/a "+Nombre+"--");
@@ -68,17 +70,18 @@ public class MenuConsultas {
          System.out.println("Ésta opción permite calcular el promedio de notas ");
          System.out.println("según el semestre indicado");
          System.out.println();
-         System.out.println("Presione 1, Para el primer semestre [Matematicas,lenguaje e Historia.]");
-         System.out.println("Presione 2, Para el segundo semestre[ciencias naturales, biologia y quimica.]");
+         System.out.println("Presione 1, Para el primer semestre [Matemáticas,Lenguaje y Comunicación e Historia.]");
+         System.out.println("Presione 2, Para el segundo semestre[Ciencias Naturales, Biologia y Química.]");
          System.out.print  ("Seleccione el Semestre: ");
          semestre = captura.nextInt();
          
-         //En el primer semestre se ingresaran las notas para las asignaturas de Matematicas,lenguaje e Historia
+//En el primer semestre se ingresaran las notas para las asignaturas de Matematicas,lenguaje e Historia
+//Estructura CASE
         switch (semestre) {
         case 1:
-                System.out.print("Ingrese la cantidad de notas para la asignatura de Matematicas: ");
+                System.out.print("Ingrese la cantidad de notas para la asignatura de Matemáticas: ");
                 cantidad = captura.nextInt();
-                
+//Estructuras FOR
                 System.out.println("use comas para separar decimales: ");
                 for (int n = 1; n <= cantidad; n++) {
                     System.out.print("nota°" + n + ": ");
@@ -86,10 +89,10 @@ public class MenuConsultas {
                     suma1 = (suma1 + numero);
                     promedio1 = (suma1 / cantidad);
                 }
-                System.out.println("EL promedio de las notas ingresadas para matemáticas es: " + promedio1);
+                System.out.println("El promedio de las notas ingresadas para Matemáticas es: " + promedio1);
                 System.out.println();
                 
-                System.out.print("Ingrese la cantidad de notas para la asignatura de Lenguaje y comunicación: ");
+                System.out.print("Ingrese la cantidad de notas para la asignatura de Lenguaje y Comunicación: ");
                 cantidad = captura.nextInt();
                 
                 System.out.println("use comas para separar decimales: ");
@@ -99,7 +102,7 @@ public class MenuConsultas {
                     suma2 = (suma2 + numero);
                     promedio2 = (suma2 / cantidad);
                 }
-                System.out.println("EL promedio de las notas ingresadas para lenguaje y comunicación es: " + promedio2);
+                System.out.println("El promedio de las notas ingresadas para Lenguaje y Comunicación es: " + promedio2);
                 System.out.println();
                 
                 System.out.print("Ingrese la cantidad de notas para la asignatura de Historia: ");
@@ -112,7 +115,7 @@ public class MenuConsultas {
                     suma3 = (suma3 + numero);
                     promedio3 = (suma3 / cantidad);
                 }
-                System.out.println("EL promedio de las notas ingresadas Historia es: " + promedio3);
+                System.out.println("El promedio de las notas ingresadas Historia es: " + promedio3);
                 System.out.println();
                     
 //En este punto se evaluara si el promedio de las tres asignaturas del primes semestre son superior a la nota solicitada,
@@ -121,17 +124,76 @@ public class MenuConsultas {
                 promediosemestre1= ((promedio1+promedio2+promedio3)/3);
                                 
                 if(promediosemestre1 <= 5){
-                    System.out.println(("Estimado Alumno/a ")+Nombre+(" el promedio minimo para aprobar el semestre es de 5.0"));
+                    System.out.println(("Estimado Alumno/a ")+Nombre+(" el promedio mínimo para aprobar el semestre es de 5.0"));
                     System.out.println("Su promedio es de : "+promediosemestre1);
                     System.out.println("Lo sentimos no cumple el promedio mínimo para aprobar el 1° semestre");
                 }else{
-                    System.out.println(("Estimado Alumno ")+Nombre+(" el promedio minimo para aprobar el semestre es de 5.0"));
+                    System.out.println(("Estimado Alumno ")+Nombre+(" el promedio mínimo para aprobar el semestre es de 5.0"));
                     System.out.println("Su promedio es : "+promediosemestre1);
                     System.out.println("Usted cumple con con el requisito para aprobar el 1° semestre !Felicitaciones¡");
                 }
-        break;
-        
-    }
+        break;           
+// En el segundo semestre se ingresaran las notas  para las asignaturas de ciencias naturales, biologia y quimica.
+        case 2:
+                System.out.print("Ingrese la cantidad de notas para la asignatura de  Ciencias Naturales: ");
+                cantidad = captura.nextInt();
+                
+                System.out.println("use comas para separar decimales: ");
+                for (int n = 1; n <= cantidad; n++) {
+                    System.out.print("nota°" + n + ": ");
+                    double numero = captura.nextDouble();
+                    suma4 = (suma4 + numero);
+                    promedio4 = (suma4 / cantidad);
+                }
+                System.out.println("El promedio de las notas ingresadas para Ciencias Naturales es: " + promedio4);
+                System.out.println();
+                
+                System.out.print("Ingrese la cantidad de notas para la asignatura de Biología: ");
+                cantidad = captura.nextInt();
+                
+                System.out.println("use comas para separar decimales: ");
+                for (int n = 1; n <= cantidad; n++) {
+                    System.out.print("nota°" + n + ": ");
+                    double numero = captura.nextDouble();
+                    suma5 = (suma5 + numero);
+                    promedio5 = (suma5 / cantidad);
+                }
+                System.out.println("El promedio de las notas ingresadas para Biología es: " + promedio5);
+                System.out.println();
+                
+                System.out.print("Ingrese la cantidad de notas para la asignatura de Química: ");
+                cantidad = captura.nextInt();
+                
+                System.out.println("use comas para separar decimales: ");
+                for (int n = 1; n <= cantidad; n++) {
+                    System.out.print("nota°" + n + ": ");
+                    double numero = captura.nextDouble();
+                    suma6 = (suma6 + numero);
+                    promedio6 = (suma6 / cantidad);
+                }
+                System.out.println("El promedio de las notas ingresadas Química es: " + promedio6);
+                System.out.println();
+                    
+//En este punto se evaluara si el promedio de las tres asignaturas del primes semestre son superior a la nota solicitada,
+//la cual es nota 5.de no ser asi se le indicara al alumno que esta reprobado por nota insufuciente.                    
+                
+                promediosemestre2= ((promedio4+promedio5+promedio6)/3);
+                                
+                if(promediosemestre2 <= 5){
+                    System.out.println(("Estimado Alumno ")+Alumno+(" el promedio minimo para aprobar el semestre es de 5.0"));
+                    System.out.println("Su promedio es de : "+promediosemestre2);
+                    System.out.println("Lo sentimos no cumple el promedio mínimo para aprobar el 2° semestre");
+                }else{
+                    System.out.println(("Estimado Alumno ")+Alumno+(" el promedio minimo para aprobar el semestre es de 5.0"));
+                    System.out.println("Su promedio es : "+promediosemestre2);
+                    System.out.println("Usted cumple con con el requisito para aprobar el 2° semestre !Felicitaciones¡");
+                 break;
+                }  
+             }
+//si no es alumno, no permite el acceso al programa        
+                     }if(opcion==2){
+                      System.out.println("Lo sentimos, no se permite el acceso a ex alumnos");
+                    }
    }
  }
-}
+
