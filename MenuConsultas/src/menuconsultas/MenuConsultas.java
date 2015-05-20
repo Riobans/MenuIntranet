a47102fa0114040e74c9531cbcd98073f9427df0
Riobans <riobans.urra@inacapmail.cl>
2015-05-20 02:17:27 -0300

    Segundo Envío
    Ingreso de notas por semestre

--- MenuConsultas/src/menuconsultas/MenuConsultas.java
+++ MenuConsultas/src/menuconsultas/MenuConsultas.java
@@ -31,8 +31,19 @@ public class MenuConsultas {
      int cantidad;
      
      //Variables usadas para calcular promedios
+     //Variables usadas para calcular promedios
      double promedio1 = 0;
+     double promedio2 = 0;
+     double promedio3 = 0;
+     double promedio4 = 0;
+     double promedio5 = 0;
+     double promedio6 = 0;
      double suma1 = 0;
+     double suma2 = 0;
+     double suma3 = 0;
+     double suma4 = 0;
+     double suma5 = 0;
+     double suma6 = 0;
      double promediosemestre1;
      double promediosemestre2;
      
@@ -61,7 +72,66 @@ public class MenuConsultas {
          System.out.println("Presione 2, Para el segundo semestre[ciencias naturales, biologia y quimica.]");
          System.out.print  ("Seleccione el Semestre: ");
          semestre = captura.nextInt();
+         
+         //En el primer semestre se ingresaran las notas para las asignaturas de Matematicas,lenguaje e Historia
+        switch (semestre) {
+        case 1:
+                System.out.print("Ingrese la cantidad de notas para la asignatura de Matematicas: ");
+                cantidad = captura.nextInt();
+                
+                System.out.println("use comas para separar decimales: ");
+                for (int n = 1; n <= cantidad; n++) {
+                    System.out.print("nota°" + n + ": ");
+                    double numero = captura.nextDouble();
+                    suma1 = (suma1 + numero);
+                    promedio1 = (suma1 / cantidad);
+                }
+                System.out.println("EL promedio de las notas ingresadas para matemáticas es: " + promedio1);
+                System.out.println();
+                
+                System.out.print("Ingrese la cantidad de notas para la asignatura de Lenguaje y comunicación: ");
+                cantidad = captura.nextInt();
+                
+                System.out.println("use comas para separar decimales: ");
+                for (int n = 1; n <= cantidad; n++) {
+                    System.out.print("nota°" + n + ": ");
+                    double numero = captura.nextDouble();
+                    suma2 = (suma2 + numero);
+                    promedio2 = (suma2 / cantidad);
+                }
+                System.out.println("EL promedio de las notas ingresadas para lenguaje y comunicación es: " + promedio2);
+                System.out.println();
+                
+                System.out.print("Ingrese la cantidad de notas para la asignatura de Historia: ");
+                cantidad = captura.nextInt();
+                
+                System.out.println("use comas para separar decimales: ");
+                for (int n = 1; n <= cantidad; n++) {
+                    System.out.print("nota°" + n + ": ");
+                    double numero = captura.nextDouble();
+                    suma3 = (suma3 + numero);
+                    promedio3 = (suma3 / cantidad);
+                }
+                System.out.println("EL promedio de las notas ingresadas Historia es: " + promedio3);
+                System.out.println();
+                    
+//En este punto se evaluara si el promedio de las tres asignaturas del primes semestre son superior a la nota solicitada,
+//la cual es nota 5.de no ser asi se le indicara al alumno que esta reprobado por nota insufuciente.                    
+                
+                promediosemestre1= ((promedio1+promedio2+promedio3)/3);
+                                
+                if(promediosemestre1 <= 5){
+                    System.out.println(("Estimado Alumno/a ")+Nombre+(" el promedio minimo para aprobar el semestre es de 5.0"));
+                    System.out.println("Su promedio es de : "+promediosemestre1);
+                    System.out.println("Lo sentimos no cumple el promedio mínimo para aprobar el 1° semestre");
+                }else{
+                    System.out.println(("Estimado Alumno ")+Nombre+(" el promedio minimo para aprobar el semestre es de 5.0"));
+                    System.out.println("Su promedio es : "+promediosemestre1);
+                    System.out.println("Usted cumple con con el requisito para aprobar el 1° semestre !Felicitaciones¡");
+                }
+        break;
         
     }
    }
+ }
 }
